5bdd36d6e6afb59e754c60bf7e554e6f5084053b
4l1fe <4l1fe@users.noreply.github.com>
2016-01-25 04:59:26 +0300

    рефакторинг

--- sorting/shell.c
+++ sorting/shell.c
@@ -1,46 +1,45 @@
 #include <stdio.h>
 #include "common.h"
 
-#define SIZE 7
+#define SIZE 8
 
-int array[SIZE] = {4,6,3,2,1,9,7};
+int array[SIZE] = {35,33,42,10,14,19,27,44};
 
 void shellSort() {
-    int inner, outer, insertion_value;
-    int i = 0, interval = 1, elements = SIZE;
+    int left, right, insertion_value;
+    int step = 0, interval = 1;
 
-    while(interval <= elements/3) {
+    while(interval <= SIZE/3) {
         interval = interval * 3 + 1;
     }
 
     while(interval > 0) {
-        printf("iteration %d#:",i);
+        printf("iteration %d#:",step);
         display(array, SIZE);
 
-        for(outer=interval; outer<elements; outer++) {
-            insertion_value = array[outer];
-            inner = outer;
+        for(right=interval; right<SIZE; right++) {
+            insertion_value = array[right];
+            left = right;
 
-            while(inner>interval-1 && array[inner-interval] >= insertion_value) {
-                array[inner] = array[inner-interval];
-                inner -= interval;
-                printf(" item moved :%d\n", array[inner]);
+            while(left>interval-1 && array[left-interval] >= insertion_value) {
+                array[left] = array[left-interval];
+                left -= interval;
+                printf(" item moved :%d\n", array[left]);
             }
 
-            array[inner] = insertion_value;
-            printf(" item inserted :%d, at position :%d\n", insertion_value, inner);
+            array[left] = insertion_value;
+            printf(" item inserted :%d, at position :%d\n", insertion_value, left);
         }
 
         interval = (interval -1) /3;
-        i++;
+        step++;
     }
 }
 
-int main() {
+void main() {
     printf("Input Array: ");
     display(array, SIZE);
     shellSort();
     printf("Output Array: ");
     display(array, SIZE);
-    return 1;
 }
